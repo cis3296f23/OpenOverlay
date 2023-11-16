95fb84431ef76787208780408b09f38d61b9dd57
Elijah Bigham <elijahbigham@gmail.com>
2023-11-15 19:54:41 -0500

    added a function which optains the current summoner.

---
+++ getCurrentSummoner.js
@@ -0,0 +1,16 @@
+var leagueConnect = require("league-connect");
+async function getCurrentSummoner() {
+  const credentials = await leagueConnect.authenticate();
+  const response = await leagueConnect.createHttp1Request(
+    {
+      method: "GET",
+      url: "/lol-summoner/v1/current-summoner",
+    },
+    credentials
+  );
+  return response.json();
+}
+async function getCurrentSummonerName() {
+  var currentSummoner = await getCurrentSummoner();
+  return currentSummoner.displayName;
+}
