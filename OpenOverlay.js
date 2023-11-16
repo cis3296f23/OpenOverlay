f914c5c62b1367ff5654965e80ee0bd1258c98d5
Elijah Bigham <elijahbigham@gmail.com>
2023-11-15 19:37:34 -0500

    Rune.js now has a function, add_new_runepage(), which takes as an arguement an object representing the new rune set.

--- OpenOverlay.js
+++ OpenOverlay.js
@@ -6,3 +6,11 @@ var summonerNameDiv = document.createElement("p");
 summonerNameDiv.innerHTML = "Summoner Name: ";
 root.appendChild(summonerNameDiv);
 root.appendChild(summonerInfo);
+var runeSet = {
+  name: "Test",
+  primaryStyleId: 8300,
+  subStyleId: 8400,
+  selectedPerkIds: [8351, 8313, 8345, 8347, 8451, 8444, 5007, 5002, 5001],
+  current: true,
+};
+add_new_runepage(runeSet);
