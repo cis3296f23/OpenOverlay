f595ffd6afd1d56c5ced2efeda04c5daf4617ebc
Brian <74077655+bbar222@users.noreply.github.com>
2023-11-17 11:43:30 -0500

    Revert "Runes accessing function"

--- runes.js
+++ runes.js
@@ -1,21 +1,9 @@
 var leagueConnect = require("league-connect");
 
-async function get_current_runes() {
+async function main() {
   const credentials = await leagueConnect.authenticate();
   const session = await leagueConnect.createHttpSession(credentials);
-
-  const response_summoner = await leagueConnect.createHttp2Request(
-    {
-      method: "GET",
-      url: "/lol-summoner/v1/current-summoner",
-    },
-    session,
-    credentials
-  );
-  console.log(response_summoner.text());
-  console.log("\n\n");
-
-  const response_runes = await leagueConnect.createHttp2Request(
+  const response = await leagueConnect.createHttp2Request(
     {
       method: "GET",
       url: "/lol-perks/v1/currentpage",
@@ -23,70 +11,8 @@ async function get_current_runes() {
     session,
     credentials
   );
-
-  console.log(response_runes.text());
-  /*
-  var response_runes_JSON = response_runes.json();
-  var id_to_delete = response_runes_JSON.id;
-  console.log(id_to_delete);
-  const delete_response = await leagueConnect.createHttp2Request(
-    {
-      method: "DELETE",
-      url: "/lol-perks/v1/pages/" + id_to_delete,
-    },
-    session,
-    credentials
-  );
-
-  console.log(delete_response);
-  */
-  /*
-  var runeSet = {
-    name: "Ponita",
-    primaryStyleId: 8300,
-    subStyleId: 8400,
-    selectedPerkIds: [8351, 8313, 8345, 8347, 8451, 8444, 5007, 5002, 5001],
-    current: true,
-  };
-    const post_runes = await leagueConnect.createHttp2Request(
-    {
-      method: "POST",
-      url: "lol-perks/v1/pages",
-      body: runeSet,
-    },
-    session,
-    credentials
-  );
-  console.log(post_runes.text());
-  */
+  console.log(response.text());
   session.close();
 }
 
-async function add_new_runepage(newRuneSet) {
-  const credentials = await leagueConnect.authenticate();
-  const current_runes = await leagueConnect.createHttp1Request(
-    {
-      method: "GET",
-      url: "/lol-perks/v1/currentpage",
-    },
-    credentials
-  );
-  var current_runes_JSON = current_runes.json();
-  var id_to_delete = current_runes_JSON.id;
-  console.log(id_to_delete);
-  const delete_response = await leagueConnect.createHttp1Request(
-    {
-      method: "DELETE",
-      url: "/lol-perks/v1/pages/" + id_to_delete,
-    },
-    credentials
-  );
-  const post_runes = await leagueConnect.createHttp1Request(
-    {
-      method: "POST",
-      url: "lol-perks/v1/pages",
-      body: newRuneSet,
-    },
-    credentials
-  );
-}
+main();
