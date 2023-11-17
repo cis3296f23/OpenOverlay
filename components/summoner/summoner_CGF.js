67e1ffb53dd39e338e45c010cc48cb83ae325343
Aidan Scharnikow <tup95685@temple.edu>
2023-11-17 04:52:19 -0500

    Added other functionalities currently implemeneted for summoner to the summoner_CGF

--- components/summoner/summoner_CGF.js
+++ components/summoner/summoner_CGF.js
@@ -1,6 +1,15 @@
 function summoner_CGF(summonerName) {
     var summonerInfoDiv = document.createElement('div');
+
     summonerInfoDiv.appendChild(getSummoner({summonerName: summonerName}));
+
+    getRank({summonerName: summonerName}, function(rankContainer) {
+        summonerInfoDiv.appendChild(rankContainer);
+    });
+
+    getRankFlex({}, function(rankContainer) {
+        summonerInfoDiv.appendChild(rankContainer);
+    });
 
     return summonerInfoDiv
 }
