25119334feaa951084b1771912434a9b6492988b
Elijah Bigham <elijahbigham@gmail.com>
2023-11-30 17:12:06 -0500

    updated items code and test code for items

--- items/itemTest.js
+++ items/itemTest.js
@@ -1,9 +1,29 @@
+const { add } = require("lodash");
+
 const get_item_sets = require("./items.js").get_item_sets;
+const add_new_itemset = require("./items.js").add_new_itemset;
 
 async function main() {
   var response = await get_item_sets();
   var item_sets = response.itemSets;
   printJson(response);
+
+  var item_set = {
+    title: "Test set",
+    blocks: [
+      {
+        items: [{ count: 2, id: 6653 }],
+        type: "Test Block 1:",
+        map: "any",
+        mode: "any",
+        sortrank: 0,
+        type: "custom",
+      },
+    ],
+  };
+
+  var add_response = await add_new_itemset(item_set);
+  printJson(add_response);
 }
 main();
 
--- items/items.js
+++ items/items.js
@@ -1,19 +1,25 @@
 var leagueConnect = require("league-connect");
 const getCurrentSummoner = require("../getCurrentSummoner.js");
 
-async function add_new_itemset(newRuneSet) {
+async function add_new_itemset(newItemSet) {
   const credentials = await leagueConnect.authenticate();
-  var current_runes_JSON = await get_item_sets(credentials);
-  var id_to_delete = current_runes_JSON.id;
-  console.log(id_to_delete);
-  const post_runes = await leagueConnect.createHttp1Request(
+  //var current_runes_JSON = await get_item_sets(credentials);
+  //var id_to_delete = current_runes_JSON.id;
+  //console.log(id_to_delete);
+  var currentSummoner = await getCurrentSummoner();
+  var currentSummonerID = currentSummoner.summonerId;
+  var current_item_sets = await get_item_sets();
+  current_item_sets.itemSets.push(newItemSet);
+
+  const post_items_request = await leagueConnect.createHttp1Request(
     {
-      method: "POST",
-      url: "lol-perks/v1/pages",
-      body: newRuneSet,
+      method: "PUT",
+      url: `/lol-item-sets/v1/item-sets/${currentSummonerID}/sets`,
+      body: current_item_sets,
     },
     credentials
   );
+  return post_items_request.json();
 }
 async function get_item_sets(credentials) {
   if (credentials === undefined) {
